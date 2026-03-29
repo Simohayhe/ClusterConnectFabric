764beeb8aea8a3fb8b430612798174b58303b449
Simohayhe <166674587+Simohayhe@users.noreply.github.com>
2026-03-30 06:02:31 +0900

    Initial commit

---
+++ src/main/java/net/simohaya/clusterconnect_fabric/Clusterconnect_fabric.java
@@ -0,0 +1,10 @@
+package net.simohaya.clusterconnect_fabric;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Clusterconnect_fabric implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
